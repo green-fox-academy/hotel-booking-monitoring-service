826cc38a057006d21a577c73e57c3b0021e29c7a
Andras Podobni <podobni.andras@gmail.com>
2017-06-08 14:11:40 +0200

    corret test

--- src/main/java/com/greenfox/kryptonite/projectx/controller/MainRestController.java
+++ src/main/java/com/greenfox/kryptonite/projectx/controller/MainRestController.java
@@ -21,4 +21,8 @@ public class MainRestController {
       return new Status("ok", "error");
     }
   }
+
+  public MainRestController(StatusRepository statusRepo) {
+    this.statusRepo = statusRepo;
+  }
 }
