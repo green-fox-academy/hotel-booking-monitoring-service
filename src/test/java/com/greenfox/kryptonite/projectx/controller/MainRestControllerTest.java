db3d08087788d9aae1d7296a5f95bb1c4a24b23a
Balazs Stegmar <stegmarb@gmail.com>
2017-06-08 11:25:43 +0200

    feat(PrijectX): added test for restcontriller

---
+++ src/test/java/com/greenfox/kryptonite/projectx/controller/MainRestControllerTest.java
@@ -0,0 +1,53 @@
+package com.greenfox.kryptonite.projectx.controller;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import java.nio.charset.Charset;
+
+import com.greenfox.kryptonite.projectx.ProjectxApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ProjectxApplication.class)
+@WebAppConfiguration
+@EnableWebMvc
+public class MainRestControllerTest {
+
+
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+          MediaType.APPLICATION_JSON.getSubtype(),
+          Charset.forName("utf8"));
+
+  private MockMvc mockMvc;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  @Before
+  public void setup() throws Exception {
+    this.mockMvc = webAppContextSetup(webApplicationContext).build();
+  }
+
+  @Test
+  public void testGetEndpoint() throws Exception {
+    mockMvc.perform(post("/heathbeat"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(contentType))
+            .andExpect(jsonPath("$.status", is("ok")));
+  }
+}
