8593f3da0e441f35f0ece1adf9234f08b82f2491
norbert barics <norbert.barics@gmail.com>
2017-06-08 12:25:02 +0200

    updated status with error field

--- src/main/java/com/greenfox/kryptonite/projectx/model/Status.java
+++ src/main/java/com/greenfox/kryptonite/projectx/model/Status.java
@@ -1,16 +1,26 @@
 package com.greenfox.kryptonite.projectx.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 public class Status {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
   private String status;
+  private String datebase;
 
-  public Status(String status) {
+  public Status(String status, String datebase) {
     this.status = status;
+    this.datebase = datebase;
   }
 
   public Status() {
