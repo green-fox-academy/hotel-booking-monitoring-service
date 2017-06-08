a3c28114f93e8ba32e99b9e03a9bcaa4f6907a0d
Andras Podobni <podobni.andras@gmail.com>
2017-06-08 16:11:23 +0200

    rename Status class to Response and create another Status.class, modifies the databaseCheck method.

--- src/main/java/com/greenfox/kryptonite/projectx/model/Status.java
+++ src/main/java/com/greenfox/kryptonite/projectx/model/Status.java
@@ -1,23 +1,16 @@
 package com.greenfox.kryptonite.projectx.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-
 
 @Getter
 @Setter
 public class Status {
 
   private String status;
-  private String database;
 
-  public Status(String status, String database) {
+  public Status(String status) {
     this.status = status;
-    this.database = database;
   }
 
   public Status() {
