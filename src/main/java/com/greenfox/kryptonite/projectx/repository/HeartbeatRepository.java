df8412c859ab6e22b668eb9aa301f3330b7602be
Andras Podobni <podobni.andras@gmail.com>
2017-06-13 14:17:43 +0200

    change hearthbeat to heartbeat

--- src/main/java/com/greenfox/kryptonite/projectx/repository/HeartbeatRepository.java
+++ src/main/java/com/greenfox/kryptonite/projectx/repository/HeartbeatRepository.java
@@ -1,10 +1,10 @@
 package com.greenfox.kryptonite.projectx.repository;
 
-import com.greenfox.kryptonite.projectx.model.Hearthbeat;
+import com.greenfox.kryptonite.projectx.model.Heartbeat;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HearthbeatRepository extends CrudRepository<Hearthbeat,Boolean> {
+public interface HeartbeatRepository extends CrudRepository<Heartbeat,Boolean> {
 
 }
--- src/main/java/com/greenfox/kryptonite/projectx/service/ProjectXService.java
+++ src/main/java/com/greenfox/kryptonite/projectx/service/ProjectXService.java
@@ -2,16 +2,16 @@ package com.greenfox.kryptonite.projectx.service;
 
 import com.greenfox.kryptonite.projectx.model.Response;
 import com.greenfox.kryptonite.projectx.model.Status;
-import com.greenfox.kryptonite.projectx.repository.HearthbeatRepository;
+import com.greenfox.kryptonite.projectx.repository.HeartbeatRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProjectXService {
 
-  public Status databaseCheck(HearthbeatRepository hearthbeatRepository) {
-    if (hearthbeatRepository == null) {
+  public Status databaseCheck(HeartbeatRepository heartbeatRepository) {
+    if (heartbeatRepository == null) {
       return new Status("ok");
-    } else if(hearthbeatRepository.count() > 0) {
+    } else if(heartbeatRepository.count() > 0) {
       return new Response("ok", "ok");
     } else {
       return new Response("ok", "error");
