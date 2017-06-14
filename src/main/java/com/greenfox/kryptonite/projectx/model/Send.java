71727ea6a9e89fa9465f333c54ec774a4227f57d
Andras Podobni <podobni.andras@gmail.com>
2017-06-14 16:25:16 +0200

    try to add rabbit_mq_bigwig_TX_URL for host

--- src/main/java/com/greenfox/kryptonite/projectx/model/Send.java
+++ src/main/java/com/greenfox/kryptonite/projectx/model/Send.java
@@ -1,20 +1,36 @@
 package com.greenfox.kryptonite.projectx.model;
 
 
-
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import lombok.NoArgsConstructor;
 
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @NoArgsConstructor
 public class Send {
-  private String envLog = System.getenv("RABBITMQ_BIGWIG_REST_API_URL");
+
+  private URI rabbitMqUrl;
+
+
+
+
+
+
   private final static String QUEUE_NAME = "kryptonite";
 
   public void send() throws Exception {
+
+    try {
+      rabbitMqUrl = new URI(System.getenv("RABBITMQ_BIGWIG_TX_URL"));
+    } catch(URISyntaxException e) {
+      e.getStackTrace();
+    }
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost(envLog);
+    factory.setHost(rabbitMqUrl.getHost());
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
 
