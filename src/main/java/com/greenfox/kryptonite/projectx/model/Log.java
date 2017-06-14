5f42aede5f2843336b2ad549f73f2b84282c9cdb
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-06-14 18:54:07 +0200

    update Log class

--- src/main/java/com/greenfox/kryptonite/projectx/model/Log.java
+++ src/main/java/com/greenfox/kryptonite/projectx/model/Log.java
@@ -1,5 +1,27 @@
 package com.greenfox.kryptonite.projectx.model;
 
-public class Log {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class Log {
+  private String type;
+  private String date;
+  private final String appName = "greenfox-kryptonite.herokuapp.com";
+  private String message;
+  private int returnValue;
+
+  public Log(String type, String message, int returnValue) {
+    this.date = new SimpleDateFormat("yyyy-MM-dd'T'KK:mm:ss'Z'").format(new Date());
+    this.type = type;
+    this.message = message;
+    this.returnValue = returnValue;
+  }
+
+  @Override
+  public String toString() {
+    return type +
+        " " + date +
+        " " + appName +
+        " " + message;
+  }
 }
