7d4150020bbeb9ce2db63b07ec063e0b287e431c
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-06-14 14:38:35 +0200

    create Send class

---
+++ src/main/java/com/greenfox/kryptonite/projectx/model/Send.java
@@ -0,0 +1,25 @@
+package com.greenfox.kryptonite.projectx.model;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+public class Send {
+  private final static String QUEUE_NAME = "kryptonite";
+
+  public void send() throws Exception {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("https://greenfox-kryptonite.herokuapp.com/heartbeat");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    String message = "Hello World!";
+    channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+    System.out.println(" [x] Sent '" + message + "'");
+
+    channel.close();
+    connection.close();
+  }
+
+}
