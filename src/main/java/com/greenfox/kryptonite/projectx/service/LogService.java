2a9b10d3c5c73ffdcf36c7c8b22897d6a1a34a17
norbert barics <norbert.barics@gmail.com>
2017-06-14 14:11:19 +0200

    git addid stars to logmessage

--- src/main/java/com/greenfox/kryptonite/projectx/service/LogService.java
+++ src/main/java/com/greenfox/kryptonite/projectx/service/LogService.java
@@ -7,25 +7,39 @@ public class LogService {
 
   public int debug(String message) {
     String date = new SimpleDateFormat("yyyy-MM-dd'T'KK:mm:ss'Z'").format(new Date());
+    printStars();
     System.out.println("DEBUG " + date + " greenfox-kryptonite.herokuapp.com " + message);
+    printStars();
     return 500;
   }
 
   public int info(String message) {
     String date = new SimpleDateFormat("yyyy-MM-dd'T'KK:mm:ss'Z'").format(new Date());
+    printStars();
     System.out.println ("INFO " + date + " greenfox-kryptonite.herokuapp.com " + message);
+    printStars();
     return 400;
   }
 
   public int warn(String message) {
     String date = new SimpleDateFormat("yyyy-MM-dd'T'KK:mm:ss'Z'").format(new Date());
+    printStars();
     System.err.println ("WARN " + date + " greenfox-kryptonite.herokuapp.com " + message);
+    printStars();
     return 300;
   }
 
   public int error(String message) {
     String date = new SimpleDateFormat("yyyy-MM-dd'T'KK:mm:ss'Z'").format(new Date());
+    printStars();
     System.err.println ("ERROR " + date + " greenfox-kryptonite.herokuapp.com " + message);
+    printStars();
     return 200;
+  }
+
+  public void printStars(){
+    for(int i = 0;i < 4; ++i){
+      System.out.println("*");
+    }
   }
 }
