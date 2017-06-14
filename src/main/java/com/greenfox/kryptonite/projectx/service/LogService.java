e88d2a41b7cf523a256b5bc102ea02c538b178f0
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-06-14 22:07:08 +0200

    add select println method and refactor

--- src/main/java/com/greenfox/kryptonite/projectx/service/LogService.java
+++ src/main/java/com/greenfox/kryptonite/projectx/service/LogService.java
@@ -7,21 +7,31 @@ import java.util.Date;
 public class LogService {
 
   public int log(String type, String message) {
-    Log log;
-
-    if (type.equals("DEBUG")){
-      log = new Log(type,message,500);
-    } else if (type.equals("INFO")) {
-      log = new Log(type,message,400);
-    } else if (type.equals("WARN")) {
-      log = new Log(type,message,300);
-    } else if (type.equals("ERROR")) {
-      log = new Log(type,message,200);
-    } else {
-      log = new Log();
-    }
-    System.out.println(log);
+    Log log = createLog(type,message);
+    selectPrintln(log);
     return log.getReturnValue();
+  }
+
+  private Log createLog(String type, String message) {
+    if (type.equals("DEBUG")){
+      return new Log(type,message,500);
+    } else if (type.equals("INFO")) {
+      return new Log(type,message,400);
+    } else if (type.equals("WARN")) {
+      return new Log(type,message,300);
+    } else if (type.equals("ERROR")) {
+      return new Log(type,message,200);
+    } else {
+      return new Log();
+    }
+  }
+
+  private void selectPrintln(Log log) {
+    if (log.getType().equals("DEBUG") || log.getType().equals("INFO")) {
+      System.out.println(log);
+    } else if (log.getType().equals("WARN") || log.getType().equals("ERROR")) {
+      System.err.println(log);
+    }
   }
 
 
