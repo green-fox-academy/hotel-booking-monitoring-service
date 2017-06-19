5250d78cfda6353464e3c7b55c1c873b98a0bd25
Andras Podobni <podobni.andras@gmail.com>
2017-06-19 12:48:08 +0200

    try again

--- src/main/java/com/greenfox/kryptonite/projectx/controller/MainRestController.java
+++ src/main/java/com/greenfox/kryptonite/projectx/controller/MainRestController.java
@@ -7,6 +7,7 @@ import com.greenfox.kryptonite.projectx.model.Status;
 import com.greenfox.kryptonite.projectx.repository.HeartbeatRepository;
 import com.greenfox.kryptonite.projectx.service.ProjectXService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +22,13 @@ public class MainRestController {
 
   @Autowired
   private ProjectXService projectXService;
+
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(code = HttpStatus.I_AM_A_TEAPOT)
+  public Log errorHandling (HttpServletRequest e) {
+    projectXService.endpointLogger(e.getRequestURI());
+    return projectXService.endpointLogger(e.getRequestURI());
+  }
 
 
   @RequestMapping(value = "/heartbeat", method = RequestMethod.GET)
