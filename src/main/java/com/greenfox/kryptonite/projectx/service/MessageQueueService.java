5407aa58dc893c71b1fbf473c8ab587b455a76d3
Balazs Stegmar <stegmarb@gmail.com>
2017-06-20 17:07:00 +0200

    feat(ProjectX): refactor messageQueueService class

--- src/main/java/com/greenfox/kryptonite/projectx/service/MessageQueueService.java
+++ src/main/java/com/greenfox/kryptonite/projectx/service/MessageQueueService.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.autoconfigure.jms.JmsProperties;
+import org.springframework.stereotype.Service;
 
 
 import java.io.IOException;
@@ -14,18 +15,16 @@ import java.net.URISyntaxException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+@Service
 @Getter
 @Setter
-@NoArgsConstructor
 public class MessageQueueService {
 
   private final String RABBIT_MQ_URL = System.getenv("RABBITMQ_BIGWIG_RX_URL");
-  private URI rabbitMqUrl;
   private final static String QUEUE_NAME = "kryptonite2";
   private static final String EXCHANGE_NAME = "log";
-  Message jsonMessage = new Message();
+  private Message jsonMessage = new Message();
   private String temporaryMessage = "Shit";
-  static final ExecutorService threadPool = Executors.newCachedThreadPool();
 
   public void send(String message) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
@@ -34,6 +33,7 @@ public class MessageQueueService {
     Channel channel = connection.createChannel();
     channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
     channel.basicPublish("", QUEUE_NAME, null, jsonMessage.sendJsonMessage(message).getBytes("UTF-8"));
+
     System.out.println(" [x] Sent '" + message + "'");
 
     channel.close();
@@ -45,10 +45,9 @@ public class MessageQueueService {
     factory.setUri(RABBIT_MQ_URL);
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
-    GetResponse getResponse = channel.basicGet(QUEUE_NAME, false);
-    setTemporaryMessage(new String(getResponse.getBody()));
-//    channel.close();
-//    connection.close();
+//    GetResponse getResponse = channel.basicGet(QUEUE_NAME, false);
+//    setTemporaryMessage(new String(getResponse.getBody()));
+
     channel.basicConsume(QUEUE_NAME, false, new DefaultConsumer(channel){
       @Override
       public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
@@ -62,34 +61,11 @@ public class MessageQueueService {
         }
       }
     });
+
     System.out.println("Ready to receive messages!");
-    while(true){}
 
-
-//    channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
-//    String queueName = channel.queueDeclare().getQueue();
-//    channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "");
-//
-//    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-//
-//    Consumer consumer = new DefaultConsumer(channel) {
-//      @Override
-//      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
-//              throws IOException {
-//        final String message = new String(body, "UTF-8");
-//        System.out.println(message);
-//        Runnable runnable = new Runnable() {
-//          @Override
-//          public void run() {
-//            setTemporaryMessage(message);
-//          }
-//        };
-//        threadPool.submit(runnable);
-//        System.out.println(" [x] Received '" + jsonMessage.receiveJsonMessage(message).getMessage() + "'");
-//      }
-//    };
-//    channel.basicConsume(QUEUE_NAME, true, consumer);
-//    return temporaryMessage;
+    channel.close();
+    connection.close();
   }
 
   public void setTemporaryMessage(String temporaryMessage) {
