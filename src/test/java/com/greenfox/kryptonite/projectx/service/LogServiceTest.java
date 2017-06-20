27be3071e8711d2e1e77fdf802335df574bffafa
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-06-20 10:52:59 +0200

    remove tests for logservice class

--- src/test/java/com/greenfox/kryptonite/projectx/service/LogServiceTest.java
+++ src/test/java/com/greenfox/kryptonite/projectx/service/LogServiceTest.java
@@ -10,37 +10,14 @@ import static org.junit.Assert.assertEquals;
 
 public class LogServiceTest {
 
-  private LogService logging;
   private Logger logger = LogManager.getLogger(this.getClass());
 
-
-  @Before
-  public void setup() throws Exception {
-    this.logging = new LogService();
-  }
-
   @Test
-  public void testDebugLogging() {
+  public void testAllLogs() {
     logger.debug("This is a debug message");
-    assertEquals(logging.log("DEBUG","message").getReturnValue(), 500);
-  }
-
-  @Test
-  public void testInfoLogging() {
     logger.info("This is an info message");
-    assertEquals(logging.log("INFO","message").getReturnValue(), 400);
-  }
-
-  @Test
-  public void testWarnLogging() {
     logger.warn("This is a warn message");
-    assertEquals(logging.log("WARN","message").getReturnValue(), 300);
-  }
-
-  @Test
-  public void testErrorLogging() {
     logger.error("This is an error message");
-    assertEquals(logging.log("ERROR","message").getReturnValue(), 200);
   }
 
 }
