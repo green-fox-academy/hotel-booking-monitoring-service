6ab430678686ef424d550ccbd020d344c5e47286
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-06-21 11:17:47 +0200

    delete tests for old logging

--- src/test/java/com/greenfox/kryptonite/projectx/service/LogServiceTest.java
+++ src/test/java/com/greenfox/kryptonite/projectx/service/LogServiceTest.java
@@ -1,40 +1,17 @@
 package com.greenfox.kryptonite.projectx.service;
 
-import com.greenfox.kryptonite.projectx.model.Log;
-import com.greenfox.kryptonite.projectx.model.Timestamp;
-import org.junit.Before;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import static org.junit.Assert.assertEquals;
 
 public class LogServiceTest {
-
-  private LogService logging;
-
-  @Before
-  public void setup() throws Exception {
-    this.logging = new LogService();
-
-  }
+  private Logger logger = LogManager.getLogger(this.getClass());
 
   @Test
-  public void testDebugLogging() {
-    assertEquals(logging.log("DEBUG","message").getReturnValue(), 500);
-  }
-
-  @Test
-  public void testInfoLogging() {
-    assertEquals(logging.log("INFO","message").getReturnValue(), 400);
-  }
-
-  @Test
-  public void testWarnLogging() {
-    assertEquals(logging.log("WARN","message").getReturnValue(), 300);
-  }
-
-  @Test
-  public void testErrorLogging() {
-    assertEquals(logging.log("ERROR","message").getReturnValue(), 200);
+  public void testAllLogs() {
+    logger.debug("This is a debug message");
+    logger.info("This is an info message");
+    logger.warn("This is a warn message");
+    logger.error("This is an error message");
   }
 }
