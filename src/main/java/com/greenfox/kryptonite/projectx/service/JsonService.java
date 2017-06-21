582a5cbcb02921f7ab9d027a7e809c5e2953a6c4
norbert barics <norbert.barics@gmail.com>
2017-06-21 16:27:17 +0200

    test read and write methods

--- src/main/java/com/greenfox/kryptonite/projectx/service/JsonService.java
+++ src/main/java/com/greenfox/kryptonite/projectx/service/JsonService.java
@@ -3,23 +3,23 @@ package com.greenfox.kryptonite.projectx.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.greenfox.kryptonite.projectx.model.Message;
-import com.greenfox.kryptonite.projectx.model.Service;
 import com.greenfox.kryptonite.projectx.model.Services;
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.NoArgsConstructor;
 
+
+@NoArgsConstructor
 public class JsonService {
 
   private static final String DATAPATH = "monitoring-services.json";
   private static final String ERROR = "SYNTAX ERROR: ";
 
-  static Services readFiles() throws IOException {
+  public Services readFiles() throws IOException {
     ObjectMapper mapper = new ObjectMapper();
     List<String> rawLines = new ArrayList<>();
     Path myPath = Paths.get(DATAPATH);
@@ -37,16 +37,20 @@ public class JsonService {
     return mapper.readValue(fileContentConvertedToJson.toString(), Services.class);
   }
 
-  static void writeToFile(Services servicesJson) throws JsonProcessingException {
+  public boolean writeToFile(Services servicesJson) throws JsonProcessingException {
+    boolean runTimeChecker = true;
+
     Path myPath = Paths.get(DATAPATH);
     List<String> dataString = new ArrayList<>();
     ObjectMapper mapper = new ObjectMapper();
     dataString.add(mapper.writeValueAsString(servicesJson));
-    
+
     try {
       Files.write(myPath, dataString);
     } catch (IOException ex) {
       System.out.println(ERROR + "WRITE");
+      runTimeChecker = false;
     }
+    return runTimeChecker;
   }
 }
