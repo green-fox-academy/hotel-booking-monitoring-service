59de612a7a744f033aeeb288fb17b60dfec82377
norbert barics <norbert.barics@gmail.com>
2017-06-22 13:41:27 +0200

    add tostring annotations to everywhere

--- src/main/java/com/greenfox/kryptonite/projectx/model/Services.java
+++ src/main/java/com/greenfox/kryptonite/projectx/model/Services.java
@@ -4,10 +4,12 @@ import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class Services {
 
   List<Service> services;
