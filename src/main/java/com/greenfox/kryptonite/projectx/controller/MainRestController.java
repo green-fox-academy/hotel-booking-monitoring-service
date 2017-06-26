084173ca09537da061dcaceb388f85bac2778aca
norbert barics <norbert.barics@gmail.com>
2017-06-26 11:59:27 +0200

    add pageviews endpoint

--- src/main/java/com/greenfox/kryptonite/projectx/controller/MainRestController.java
+++ src/main/java/com/greenfox/kryptonite/projectx/controller/MainRestController.java
@@ -1,9 +1,9 @@
 package com.greenfox.kryptonite.projectx.controller;
 
 
-
 import com.greenfox.kryptonite.projectx.model.HotelServiceStatusList;
 import com.greenfox.kryptonite.projectx.model.BookingStatus;
+import com.greenfox.kryptonite.projectx.model.pageviews.PageViewFormat;
 import com.greenfox.kryptonite.projectx.repository.HeartbeatRepository;
 import com.greenfox.kryptonite.projectx.service.MonitoringService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,12 +29,21 @@ public class MainRestController {
   }
 
   @RequestMapping(value = "/monitor", method = RequestMethod.GET)
-  public HotelServiceStatusList monitor() throws IOException{
-  return monitoringService.monitoring();
+  public HotelServiceStatusList monitor() throws IOException {
+    return monitoringService.monitoring();
+  }
+
+  @GetMapping(value = "/pageviews")
+  public PageViewFormat pageview() {
+    PageViewFormat pageViewFormat = new PageViewFormat();
+    
+
+    return pageViewFormat;
   }
 
   @RequestMapping(value = "/{pathVariable}")
-  public BookingStatus endpointLogger(@PathVariable(name = "pathVariable") String pathVariable) throws Exception {
+  public BookingStatus endpointLogger(@PathVariable(name = "pathVariable") String pathVariable)
+      throws Exception {
     monitoringService.endpointLogger(pathVariable);
     return monitoringService.databaseCheck(heartbeatRepository);
   }
