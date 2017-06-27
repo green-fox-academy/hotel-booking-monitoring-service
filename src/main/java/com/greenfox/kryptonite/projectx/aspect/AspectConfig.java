dda99904cc3d463b5aa26c70f8cc16b6774b35a0
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-06-27 10:26:08 +0200

    add Aspect with one Before method

---
+++ src/main/java/com/greenfox/kryptonite/projectx/aspect/AspectConfig.java
@@ -0,0 +1,16 @@
+package com.greenfox.kryptonite.projectx.aspect;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectConfig {
+
+  @Bean
+  public LoggingAspect loggingAspect() {
+    return new LoggingAspect();
+  }
+
+}
--- src/main/java/com/greenfox/kryptonite/projectx/aspect/LoggingAspect.java
+++ src/main/java/com/greenfox/kryptonite/projectx/aspect/LoggingAspect.java
@@ -1,9 +1,28 @@
 package com.greenfox.kryptonite.projectx.aspect;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
 
 @Aspect
+@Component
+@Getter
+@Setter
+@NoArgsConstructor
 public class LoggingAspect {
+  private Logger logger = LogManager.getLogger(this.getClass());
 
+  @Before("execution(* com.greenfox.kryptonite.projectx.controller.MainRestController+.*(..))")
+  public void LoggingAdvice() {
+    logger.info("Hello");
+  }
 
 }
