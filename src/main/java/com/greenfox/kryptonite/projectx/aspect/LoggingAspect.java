b49fa72302845d6f8e30685c392e5fba0a78a588
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-06-27 11:03:49 +0200

    delete unnecessary config class

--- src/main/java/com/greenfox/kryptonite/projectx/aspect/LoggingAspect.java
+++ src/main/java/com/greenfox/kryptonite/projectx/aspect/LoggingAspect.java
@@ -5,10 +5,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 
