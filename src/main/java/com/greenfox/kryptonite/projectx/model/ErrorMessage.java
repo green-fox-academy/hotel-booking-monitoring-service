9da8511646cbbe8f757ae6d95e7b955db2797b5b
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-06-28 11:39:41 +0200

    create errormessage class

---
+++ src/main/java/com/greenfox/kryptonite/projectx/model/ErrorMessage.java
@@ -0,0 +1,16 @@
+package com.greenfox.kryptonite.projectx.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+  private String status;
+  private String message;
+
+}
