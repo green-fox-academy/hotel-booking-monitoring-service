2d4f944b37fe8e2b3d0c1ba704f5f689f3d23375
norbert barics <norbert.barics@gmail.com>
2017-06-29 08:51:12 +0200

    add link classes

--- src/main/java/com/greenfox/kryptonite/projectx/model/pageviews/LinksWithNextField.java
+++ src/main/java/com/greenfox/kryptonite/projectx/model/pageviews/LinksWithNextField.java
@@ -9,7 +9,8 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Links {
-
-  String self;
+public class LinksWithNextField extends Links {
+  String next;
+  String last;
+  String prev;
 }
--- src/main/java/com/greenfox/kryptonite/projectx/model/pageviews/LinksWithPrevField.java
+++ src/main/java/com/greenfox/kryptonite/projectx/model/pageviews/LinksWithPrevField.java
@@ -9,7 +9,8 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Links {
-
-  String self;
+public class LinksWithPrevField extends Links {
+  String next;
+  String last;
+  String prev;
 }
