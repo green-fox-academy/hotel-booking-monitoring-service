1a158826d73c68acb75ecee78f0784a207f82b43
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-07-04 16:13:37 +0200

    create basic pageable endpoint for pageviews

--- src/main/java/com/greenfox/kryptonite/projectx/controller/MainRestController.java
+++ src/main/java/com/greenfox/kryptonite/projectx/controller/MainRestController.java
@@ -3,14 +3,18 @@ package com.greenfox.kryptonite.projectx.controller;
 
 import com.greenfox.kryptonite.projectx.model.hotelservices.HotelServiceStatusList;
 import com.greenfox.kryptonite.projectx.model.BookingStatus;
+import com.greenfox.kryptonite.projectx.model.pageviews.EventToDatabase;
 import com.greenfox.kryptonite.projectx.model.pageviews.PageViewFormat;
 import com.greenfox.kryptonite.projectx.repository.EventToDatabaseRepository;
 import com.greenfox.kryptonite.projectx.repository.HeartbeatRepository;
 import com.greenfox.kryptonite.projectx.service.JsonAssemblerService;
 import com.greenfox.kryptonite.projectx.service.MonitoringService;
 import com.greenfox.kryptonite.projectx.service.PageViewService;
+import java.awt.print.Pageable;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -36,6 +40,7 @@ public class MainRestController {
   private final String RABBIT_MQ_URL = System.getenv("RABBITMQ_BIGWIG_RX_URL");
   private final String EXCHANGE_NAME = "log";
   private RestTemplate restTemplate = new RestTemplate();
+  private final Integer ITEMS_PER_PAGE = 20;
 
 
   @RequestMapping(value = "/heartbeat", method = RequestMethod.GET)
@@ -55,5 +60,13 @@ public class MainRestController {
   @RequestMapping(value = "/monitor", method = RequestMethod.GET)
   public HotelServiceStatusList monitor(HttpServletRequest request) throws IOException {
     return monitoringService.monitoring(restTemplate);
+  }
+
+  @RequestMapping(value = "/newpageview")
+  public Iterable<EventToDatabase> listPageviews(@RequestParam(name = "page", required = false) Integer page) {
+    if (page == null) {
+      page = 0;
+    }
+    return eventToDatabaseRepository.findAll(new PageRequest(page,ITEMS_PER_PAGE));
   }
 }
---
+++ src/main/java/com/greenfox/kryptonite/projectx/model/pageviews/PageService.java
@@ -0,0 +1,5 @@
+package com.greenfox.kryptonite.projectx.model.pageviews;
+
+public class PageService {
+
+}
