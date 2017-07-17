ea73bed462bd4294114ec42d0e27efa7d91c279c
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-07-17 22:51:16 +0200

    (test): prepare testing environment

--- src/test/java/com/greenfox/kryptonite/projectx/service/PageServiceTest.java
+++ src/test/java/com/greenfox/kryptonite/projectx/service/PageServiceTest.java
@@ -7,6 +7,50 @@ import org.junit.Test;
 public class PageServiceTest {
 
   @Test
+  public void returnPage() throws Exception {
+  }
+
+  @Test
+  public void setPageNumber() throws Exception {
+  }
+
+  @Test
+  public void createListOfFilteredPageViews() throws Exception {
+  }
+
+  @Test
+  public void createLinks() throws Exception {
+  }
+
+  @Test
+  public void createPageViewDataList() throws Exception {
+  }
+
+  @Test
+  public void setSelf() throws Exception {
+  }
+
+  @Test
+  public void setLast() throws Exception {
+  }
+
+  @Test
+  public void setNext() throws Exception {
+  }
+
+  @Test
+  public void setPrevious() throws Exception {
+  }
+
+  @Test
+  public void filterPageviews() throws Exception {
+  }
+
+  @Test
+  public void filterPageviewsByCount() throws Exception {
+  }
+
+  @Test
   public void testNewPageService() {
 
   }
