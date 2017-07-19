a5b1c08b152509c00d6594ce05fc5dea209dcb46
Balazs Stegmar <stegmarb@gmail.com>
2017-07-19 11:14:56 +0200

    feat(HotelMonitoring): refactored post method in steps endpoint

---
+++ src/main/java/com/greenfox/kryptonite/projectx/model/funnels/StepBody.java
@@ -0,0 +1,14 @@
+package com.greenfox.kryptonite.projectx.model.funnels;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StepBody {
+  private String path;
+}
